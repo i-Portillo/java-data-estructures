7370a3bc13d4d367a30019c7f76a9c20ec8d2dc2
Ismael Portillo Álvarez <Ismael Portillo lvarez>
2017-02-13 09:36:25 +0100

    Add MethodTest.java

---
+++ chap.1/MethodTest.java
@@ -0,0 +1,18 @@
+
+public class MethodTest
+{
+    public static void main(String[] args)
+    {
+        int a = 3;
+        int b = 7;
+
+        // Llamada a método
+        System.out.println(min(a, b));
+    }
+
+    // Declaración del método
+    public static int main (int x, int y)
+    {
+        return x < y ? x : y;
+    }
+}
